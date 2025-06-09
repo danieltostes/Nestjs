a699bd1246172fc5ad285470b0ed351f49115818
Daniel Tostes <daniel.tostes@gmail.com>
2025-06-09 01:17:23 -0300

    Implementação do MongoDB em memória

--- product-api/src/app.module.ts
+++ product-api/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+
+let server : MongoMemoryServer;
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/products?retryWrites=true&loadBalanced=false&serverSelectionTimeoutMS=5000&connectTimeoutMS=10000"),
+    MongooseModule.forRootAsync({
+      useFactory: async () => {
+        server = await MongoMemoryServer.create();
+        const uri = server.getUri();
+        return {
+          uri
+        }
+      }
+    }),
     ProductsModule]
 })
 export class AppModule {}
